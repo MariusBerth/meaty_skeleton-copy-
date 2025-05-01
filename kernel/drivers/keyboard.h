3b250afc193f254cd7b8d346cda6dfb084ec3d98
emile <emile@nosserf>
2025-05-01 17:01:15 +0200

    ajout d'un debut d'implementation pour un clavier

---
+++ kernel/drivers/keyboard.h
@@ -0,0 +1,3 @@
+#include <stdint.h>
+uint8_t keyboard_read(void);
+
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
 #include <kernel/fb_tty.h>
+#include <drivers/keyboard.h>
 
 void kernel_main(char* multiboot_struct) {
 	framebuffer_initialize(multiboot_struct);
 	fillrect(512, 320, 20, 20, FB_BLUE);
 	fb_terminal_setup ();
 	fb_writestring ("coucou,\nceci est le Veldos");
-	fb_writeat ("blergh", 6, 512, 320, FB_RED);
+        fb_writeat ("alergh", 6, 512, 304, FB_RED);
+	while(1)
+          {
+            fb_putchar(keyboard_read());
+            
+          };
+        fb_writeat ("alergh", 6, 512, 336, FB_RED);
+
 }
