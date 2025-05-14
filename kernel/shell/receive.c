1996a33fd6d298f9825ab9cb00d6404d343f4507
marius <maris.berthoumieux@gmail.com>
2025-05-14 14:36:42 +0200

    Ajout d'un retour à la ligne lors de l'execution de la commande receive

--- kernel/shell/receive.c
+++ kernel/shell/receive.c
@@ -1,12 +1,13 @@
 #include <drivers/network.h>
-#include <kernel/fb_tty.h>
+#include <stdint.h>
+#include <stdio.h>
 
 char buffer[1514];
 
 void receive (void) {
 	uint32_t length = rtl_poll(buffer);
 	if (length) {
-		fb_writestring (buffer);
+		printf ("%s\n",buffer);
 	};
 }
 	
