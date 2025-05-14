57cbbe4efe6faca5f13d2cb9a56cabc50fd4ffd8
marius <maris.berthoumieux@gmail.com>
2025-05-14 17:47:22 +0200

    Correction d'un bug empéchant parfois aux deux instances de se connecter entre elles

--- qemu.sh
+++ qemu.sh
@@ -6,6 +6,8 @@ qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom veldos.iso\
        	-device rtl8139,netdev=n1,mac=ff:ff:ff:ff:ff:ff \
 	-netdev socket,id=n1,listen=:1234 &
 
+sleep 1s
+
 qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom veldos.iso\
        	-device rtl8139,netdev=n2,mac=ff:ff:ff:ff:ff:ff \
 	-netdev socket,id=n2,connect=127.0.0.1:1234
